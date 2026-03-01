121d1585a95c5a3950994d68a5c51cd4b36fd5de
Pranay B <b.pranay.git@gmail.com>
2026-03-01 10:36:55 +0530

    add prime program

---
+++ Programs/prime.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+// Print prime numbers up to n
+int main() {
+    int n = 20;
+
+    printf("Prime numbers up to %d:\n", n);
+    for (int i = 2; i <= n; i++) {
+        int isPrime = 1;
+        for (int j = 2; j <= i/2; j++) {
+            if (i % j == 0) {
+                isPrime = 0;
+                break;
+            }
+        }
+        if (isPrime) {
+            printf("%d ", i);
+        }
+    }
+    return 0;
+}
