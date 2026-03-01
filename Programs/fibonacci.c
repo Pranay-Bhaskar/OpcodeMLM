ce5e929bfde115ad9e51fe5bad8065f1664477a7
Pranay B <b.pranay.git@gmail.com>
2026-03-01 10:32:24 +0530

    add fibonacci program

---
+++ Programs/fibonacci.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+// Iterative Fibonacci sequence generator
+int main() {
+    int n = 10, i;
+    int a = 0, b = 1, next;
+
+    printf("Fibonacci sequence up to %d terms:\n", n);
+    for (i = 0; i < n; i++) {
+        printf("%d ", a);
+        next = a + b;
+        a = b;
+        b = next;
+    }
+    return 0;
+}
